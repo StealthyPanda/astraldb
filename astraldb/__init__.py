de3258344f80199ebf093cf86b31d7317868c1d8
StealthyPanda <shaikm259@gmail.com>
2024-12-16 19:02:23 +0530

    Added documentation for client

--- astraldb/__init__.py
+++ astraldb/__init__.py
@@ -1,4 +1,25 @@
 
+"""
+# 🚀✨AstralDB
+
+Yet another vector database.
+
+[Quickstart guide](https://stealthypanda.github.io/astralwebpage/).
+
+Python client example:
+```python
+from astraldb import StoreClient
+with StoreClient('store-name') as store:
+    store.set('x', [1, 2, 3])
+    store.set('y', [4, 5, 6])
+    print(store.get('x')) # [1, 2, 3]
+    print(store.keys) # {'x', 'y'}
+```
+
+"""
+
+
+
 from .server import *
 from .astral import *
 from .client import *
--- astraldb/client/pythonclient.py
+++ astraldb/client/pythonclient.py
@@ -1,20 +1,65 @@
 
 import asyncio
+from typing import Iterable
+
+import numpy as np
 from ..astral import Store
 from ..utils import logger
 
 
 class StoreClient:
+    """Creates a client to interact with a store.
     
+        NOTE: Always use in a 'with' block.
+    """
     def __init__(self, store_name : str) -> None:
         self.store_name = store_name
         self.store = Store(self.store_name)
         self.store._client_mode = True
         
-        self.get = self.store.get
-        self.set = self.store.set
-        self.delete = self.store.delete
-        self.keys = self.store.keys
+    
+    def get(self, key : str) -> np.ndarray:
+        """Retrieves a single vector from the store.
+
+        Args:
+            key (str): key of the vector to retrieve.
+
+        Returns:
+            np.ndarray: vector as a numpy array.
+        """
+        return self.store.get(key)
+
+    
+    def set(self, key : str, vector : list[int | float]) -> str:
+        """Sets the value of a single vector in the store.
+
+        Args:
+            key (str): key of the vector to set.
+            vector (list[int | float]): vector to set.
+
+        Returns:
+            str: hash of the key in the store.
+        """
+        keyhash, _ = self.store.set(key, vector)
+        return keyhash
+    
+    def delete(self, key : str) -> None:
+        """Deletes a key-vector pair in the store.
+        If the key doesn't exist, it does nothing.
+
+        Args:
+            key (str): key of the vector to delete.
+        """
+        self.store.delete(key)
+    
+    @property
+    def keys(self):
+        """All the keys in this store.
+
+        Returns:
+            set[str]: all the keys as a set.
+        """
+        return self.store.keys
     
     
     def __enter__(self):
